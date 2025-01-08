145f216e6bd8cd22f22b397c8ccc6f61fe24b27e
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2025-01-09 01:37:10 +0530

    Update 07_CheckIf2StringsAreAnagramOfEachOther.java

--- Step 05 : Strings [Basic and Medium]/Lec 1: Basic and Easy String Problems/07_CheckIf2StringsAreAnagramOfEachOther.java
+++ Step 05 : Strings [Basic and Medium]/Lec 1: Basic and Easy String Problems/07_CheckIf2StringsAreAnagramOfEachOther.java
@@ -1,27 +1,56 @@
 // Question Link: https://leetcode.com/problems/valid-anagram/
 
 
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+
+//         int[] count = new int[26];
+
+//         for (int i = 0; i < s.length(); i++) {
+//             count[s.charAt(i) - 'a'] += 1;
+//         }
+
+//         for (int i = 0; i < t.length(); i++) {
+//             if (count[t.charAt(i) - 'a'] == 0) {
+//                 return false;
+//             }
+            
+//             count[t.charAt(i) - 'a'] -= 1;
+//         }
+
+//         return true;        
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+//MINE
 class Solution {
     public boolean isAnagram(String s, String t) {
+        
+        if(s.length()!=t.length()) return false;
 
-        if (s.length() != t.length()) {
-            return false;
+        int[] no = new int[26];
+        for(int i=0;i<s.length();i++){
+            no[s.charAt(i) - 'a']++;
         }
-
-        int[] count = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a'] += 1;
-        }
-
-        for (int i = 0; i < t.length(); i++) {
-            if (count[t.charAt(i) - 'a'] == 0) {
+        for(int i=0;i<t.length();i++){
+            no[t.charAt(i) - 'a']--;
+            if(no[t.charAt(i) - 'a']<0){
                 return false;
             }
-            
-            count[t.charAt(i) - 'a'] -= 1;
         }
-
-        return true;        
+        return true;
     }
 }
