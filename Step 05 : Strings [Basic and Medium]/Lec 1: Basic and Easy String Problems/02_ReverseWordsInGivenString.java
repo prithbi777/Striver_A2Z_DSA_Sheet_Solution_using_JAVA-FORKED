4c3c7ba4a2237e66578ee91b97a9364b42305072
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2025-01-09 01:22:58 +0530

    Update 02_ReverseWordsInGivenString.java

--- Step 05 : Strings [Basic and Medium]/Lec 1: Basic and Easy String Problems/02_ReverseWordsInGivenString.java
+++ Step 05 : Strings [Basic and Medium]/Lec 1: Basic and Easy String Problems/02_ReverseWordsInGivenString.java
@@ -1,40 +1,66 @@
 // Question Link: https://leetcode.com/problems/reverse-words-in-a-string/description/
 
 
-class Solution {
+// class Solution {
 
-    // Time Complexity = O(n), Space Complexity = O(n)
-    public String reverseWords(String s) {
-        Stack<String> stack = new Stack<String>();        
-        String str = "";
+//     // Time Complexity = O(n), Space Complexity = O(n)
+//     public String reverseWords(String s) {
+//         Stack<String> stack = new Stack<String>();        
+//         String str = "";
 
-        for (int i = 0; i < s.length(); i++) {
+//         for (int i = 0; i < s.length(); i++) {
 
-            if (s.charAt(i) == ' ') {
-                if (!str.isEmpty()) {  
-                    stack.push(str);
-                }
+//             if (s.charAt(i) == ' ') {
+//                 if (!str.isEmpty()) {  
+//                     stack.push(str);
+//                 }
 
-                str = "";  
-            } else {
-                str += s.charAt(i); 
-            }
-        }
+//                 str = "";  
+//             } else {
+//                 str += s.charAt(i); 
+//             }
+//         }
 
-        if (!str.isEmpty()) {
-            stack.push(str);
-        }
+//         if (!str.isEmpty()) {
+//             stack.push(str);
+//         }
 
-        String ans = "";
+//         String ans = "";
         
-        while (!stack.isEmpty()) {
-            ans += stack.pop();   
+//         while (!stack.isEmpty()) {
+//             ans += stack.pop();   
 
-            if (!stack.isEmpty()) { 
-                ans += " ";
+//             if (!stack.isEmpty()) { 
+//                 ans += " ";
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+
+
+
+
+
+
+//MINE
+class Solution {
+    public String reverseWords(String s) {
+       // Split the string by spaces and filter out empty strings
+        String[] words = s.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+
+        // Traverse the words array in reverse order
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]);
+            if (i != 0) {
+                result.append(" ");
             }
         }
-
-        return ans;
+        
+        return (result.toString()); 
     }
 }
