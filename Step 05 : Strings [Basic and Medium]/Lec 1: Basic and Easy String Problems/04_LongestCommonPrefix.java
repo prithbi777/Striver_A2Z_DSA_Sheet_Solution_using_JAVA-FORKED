5dac2339d21b56298518b1561fcb22b6309474aa
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2025-01-09 01:44:11 +0530

    Update 04_LongestCommonPrefix.java

--- Step 05 : Strings [Basic and Medium]/Lec 1: Basic and Easy String Problems/04_LongestCommonPrefix.java
+++ Step 05 : Strings [Basic and Medium]/Lec 1: Basic and Easy String Problems/04_LongestCommonPrefix.java
@@ -1,23 +1,77 @@
 // Question Link: https://leetcode.com/problems/longest-common-prefix/description/
 
+////OPTIMIZED
+// class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+//         Arrays.sort(strs);
 
+//         String s1 = strs[0];
+//         String s2 = strs[strs.length - 1];
+
+//         int index = 0;
+
+//         while (index < s1.length() && index < s2.length()){
+//             if(s1.charAt(index) == s2.charAt(index)){
+//                 index++;            
+//             } else {
+//                 break;
+//             }
+//         }
+
+//         return s2.substring(0, index);
+//     }
+// }
+
+
+
+
+
+//MINE
 class Solution {
+    // public String common(String s1, String s2){
+    //     int n = Math.min(s1.length(), s2.length());
+    //     StringBuilder sb = new StringBuilder();
+    //     for(int i=0;i<n;i++){
+    //         if(s1.charAt(i)==s2.charAt(i)){
+    //             sb.append(s1.charAt(i));
+
+    //         }
+    //         else{
+    //             break;
+    //         }
+    //     }
+    //     return sb.toString();
+    // }
     public String longestCommonPrefix(String[] strs) {
+
+
+        //OPTIMIZED CODE
         Arrays.sort(strs);
-
         String s1 = strs[0];
-        String s2 = strs[strs.length - 1];
-
+        String s2 = strs[strs.length-1];
         int index = 0;
-
-        while (index < s1.length() && index < s2.length()){
-            if(s1.charAt(index) == s2.charAt(index)){
-                index++;            
-            } else {
-                break;
-            }
+        while(index < s1.length() && index < s2.length()){
+        if(s1.charAt(index)==s2.charAt(index)){
+        index++;
+        }
+        else{
+        break;
+        }
         }
 
-        return s2.substring(0, index);
+        return s1.substring(0,index);
+
+
+
+        // // METHOD-2
+        // String result=strs[0];
+        // for(int i=1;i<strs.length;i++){
+        //     result = common(result,strs[i]);
+        // }
+
+        // return result;
+
+
+
     }
 }
