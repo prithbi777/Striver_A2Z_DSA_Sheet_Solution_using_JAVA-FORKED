0a677f88793e068aaaf561a170e196fd47a62a85
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2025-01-10 16:33:07 +0530

    Update 08_ReverseEveryWordInString.java

--- Step 05 : Strings [Basic and Medium]/Lec 2: Medium String Problems/08_ReverseEveryWordInString.java
+++ Step 05 : Strings [Basic and Medium]/Lec 2: Medium String Problems/08_ReverseEveryWordInString.java
@@ -38,3 +38,34 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//MINE
+class Solution {
+    public String reverseWords(String s) {
+       // Split the string by spaces and filter out empty strings
+        String[] words = s.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+
+        // Traverse the words array in reverse order
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]);
+            if (i != 0) {
+                result.append(" ");
+            }
+        }
+        
+        return (result.toString()); 
+    }
+}
