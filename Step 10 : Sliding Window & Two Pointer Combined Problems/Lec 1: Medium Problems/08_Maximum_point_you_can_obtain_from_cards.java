5bf73d6f39dee698649ef03f7e07891bbe3ddd3a
Prithbiraj Mahanta <90209139+prithbi777@users.noreply.github.com>
2025-01-14 18:27:11 +0530

    Create 08_Maximum_point_you_can_obtain_from_cards.java

---
+++ Step 10 : Sliding Window & Two Pointer Combined Problems/Lec 1: Medium Problems/08_Maximum_point_you_can_obtain_from_cards.java
@@ -0,0 +1,23 @@
+//problem link: https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+
+
+
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int lsum =0, rsum =0, max =0,sum =0;
+        int n = cardPoints.length;
+        for(int i=0;i<k;i++){
+            lsum = lsum + cardPoints[i];
+        }
+        max = lsum;
+        for (int i = 0; i < k; i++) {
+            lsum -= cardPoints[k-1-i];
+            rsum += cardPoints[cardPoints.length -1-i];
+            sum = lsum+rsum;
+            if(sum>max){
+                max = sum;
+            }
+        }
+        return max;
+    }
+}
